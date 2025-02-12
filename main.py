f7a37daf42cba4d1fb690362b6f3f400a52a5af3
gsonoda <guilhermesnd@gmail.com>
2025-02-12 18:42:58 -0300

    add fps limit

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from constants import *
 def main():
     print("Starting asteroids!")
     pygame.init()
-    
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -15,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
